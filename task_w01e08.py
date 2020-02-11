d5fc076e0419762f2dcaf74a86d973d6bc9b1229
zakharka <plutmercury(at)mail.ru>
2020-02-11 21:04:04 +0300

    First 1-18 exercises

---
+++ task_w01e08.py
@@ -0,0 +1,3 @@
+nuts = 38746298762973632324233242
+squirrels = 60
+print('nuts // squirrels =', nuts // squirrels)
---
+++ task_w01e18.py
@@ -0,0 +1,4 @@
+A = int(input())
+B = int(input())
+Z = int(input())
+print((A + B)*2*Z)
