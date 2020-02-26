a97a30943d53513a1a94130d9b78e588db0e6b11
zakharka <plutmercury(at)mail.ru>
2020-02-27 00:45:16 +0300

    task 05 week 03

--- w03/task_w03e05.py
+++ w03/task_w03e05.py
@@ -3,22 +3,27 @@ X = abs(int(input())) # рублей
 Y = abs(int(input())) # копеек
 
 
-def print_interest(x, y, i):
-
-    s = x *100 + y
-    s *= 1 + i / 100
-    print(int(s / 100), round(s - int(s / 100) * 100))
-    return 0
+def interest(x, y, i):
+    s = (x + y / 100) * (100 + i)
+    return int(s // 100), int(s % 100)
 
 
-def rround(x):
-    if (x - int(x)) < 0.5:
-        return int(x)
-    else:
-        return int(x) + 1
+x1, y1 = interest(X, Y, P)
+print(x1, y1)
+
+# Сравнение с альтернативным решением
+#def print_interest2(x, y, i):
+#    s = (x * 100 + y) * (1 + i / 100)
+#    return int(s // 100), int(s % 100)
+#
+#
+#for i in range(0, 101):
+#    for j in range(0, 1000):
+#        for p in range(1, 13):
+#            x1, y1 = print_interest1(j, i, p)
+#            x2, y2 = print_interest2(j, i, p)
+#            if (x1 != x2) or (y1 != y2):
+#                print(j, i, p, ":", x1, y1, end="; ")
+#                print(j, i, p, ":", x2, y2)
 
 
-print_interest(X, Y, P)
-
-print_interest(179, 0, 12)
-print_interest(1000, 0, 12)
