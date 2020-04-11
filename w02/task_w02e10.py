fc3ac9fac77c45bbaf4eb2cc1586df7ae3ebde05
zakharka <plutmercury(at)mail.ru>
2020-04-11 21:08:37 +0300

    Week 02. Добавлены комментарии к задачам

--- w02/task_w02e10.py
+++ w02/task_w02e10.py
@@ -1,3 +1,16 @@
+# Даны три целых числа. Найдите наибольшее из них (программа должна вывести
+# ровно одно целое число).
+#
+# Sample Input:
+#
+# 1
+# 2
+# 3
+#
+# Sample Output:
+#
+# 3
+
 number1 = int(input())
 number2 = int(input())
 number3 = int(input())
